f1ff8b9b8e9cc5d2c16635dbb18911098b448079
Reynaldi <38524172+reynaldiRepo@users.noreply.github.com>
2020-06-17 20:44:54 +0700

    waiting api

--- app/src/main/java/com/reynaldlancer/reynaldlancer/LoginActivity.java
+++ app/src/main/java/com/reynaldlancer/reynaldlancer/LoginActivity.java
@@ -2,13 +2,26 @@ package com.reynaldlancer.reynaldlancer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView link_to_regist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        link_to_regist = findViewById(R.id.register_link);
+        link_to_regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/reynaldlancer/reynaldlancer/RegisterActivity.java
+++ app/src/main/java/com/reynaldlancer/reynaldlancer/RegisterActivity.java
@@ -2,13 +2,28 @@ package com.reynaldlancer.reynaldlancer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    TextView link_to_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //for login link==============================================
+        link_to_login = findViewById(R.id.login_link);
+        link_to_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
+        //===============================================================
+
     }
 }
